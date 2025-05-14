06f734383445e20525c2faf9760842daea9e2ce7
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2025-05-14 23:55:06 +0500

    verify api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
+
+    const logout = () => {
+        sessionStorage.removeItem('access_token')
+        sessionStorage.removeItem('refresh_token')
+    }
     return (
-        <div className='w-screen h-screen flex items-center justify-center'>
+        <div className='w-screen h-screen flex flex-col gap-5 items-center justify-center'>
             <Link
                 to={'/login'}
                 className='py-2 px-4 bg-blue-700 hover:bg-blue-600 text-[#F3F3F3] rounded-md transition-all duration-200 ease-in-out'
             >
                 Login
             </Link>
+            <button
+                onClick={logout}
+            >
+                Logout
+            </button>
         </div>
     )
 }
